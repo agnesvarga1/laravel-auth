62e31d5cd88f23696394d0e995880bbaaef03363
agnesvarga1 <agikolyok@gmail.com>
2024-03-25 16:32:20 +0100

    db filled 5 porjects fake

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Project;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i=0; $i<5;$i++){
+            $project = new Project();
+            $project->project_name = $faker->words(3, true);
+            $project-> description = $faker->text(200);
+            $project-> image = $faker->image(null,640, 480);
+            $project-> github_repo = $faker->url();
+            $project->save();
+        }
+    }
+}
